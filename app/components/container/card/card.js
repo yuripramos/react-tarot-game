b7961b862454022a11f5ae8bd1562addd1d67bce
Yuri Ramos <yuri.pramos@gmail.com>
2017-08-08 05:55:41 -0300

    refactoring how states are updated, updated eslintrc rules, minors adjustments in style

--- app/components/container/card/card.js
+++ app/components/container/card/card.js
@@ -4,91 +4,86 @@ import ReactTooltip from 'react-tooltip';
 import style from './style.scss';
 
 class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFlipped: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.checkOneOpened = this.checkOneOpened.bind(this);
-  }
-  componentWillReceiveProps(nextprops) {
-    const { history } = this.props;
-    const last = nextprops.history[nextprops.history.length - 1];
-    if (history.length > 1) {
-      if (last.id === nextprops.keyId && !last.opened) {
-        this.setState({ isFlipped: !this.state.isFlipped }, () => {
-        });
-      }
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isFlipped: false,
+		};
+		this.handleClick = this.handleClick.bind(this);
+		this.checkOneOpened = this.checkOneOpened.bind(this);
+	}
+	componentWillReceiveProps(nextprops) {
+		if (nextprops.cardsState) {
+			this.setState({ isFlipped: false });
+		}
+	}
 
-  handleClick(e) {
-    const { keyId, historyToggleStates } = this.props;
-    if (e) {
-      e.preventDefault();
-    }
-    this.setState({ isFlipped: !this.state.isFlipped }, () => {
-      historyToggleStates(this.state.isFlipped, keyId, true);
-    });
-  }
+	handleClick(e) {
+		const { keyId, historyToggleStates } = this.props;
+		if (e) {
+			e.preventDefault();
+		}
 
-  checkOneOpened(e) {
-    if (!this.props.isShowing) {
-      this.handleClick(e);
-    }
-  }
+		this.setState({ isFlipped: !this.state.isFlipped }, () => {
+			historyToggleStates();
+		});
+	}
 
-  render() {
-    const { item, basePath, backCard, isShowing } = this.props;
-    return (
-      <div className={`col-lg-2 col-md-3 col-sm-6 ${style.card}`}>
-        <ReactCardFlip
-          isFlipped={this.state.isFlipped}
-          flipSpeedBackToFront={0.9}
-          flipSpeedFrontToBack={0.9}
-        >
-          <div
-            key="front"
-            data-tip={isShowing ?
-            `${item.name} <br/> <br/> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum` :
-            'clique para descobrir'}
-            data-multiline
-          >
-            <button
-              onClick={() => this.checkOneOpened()}
-            >
-              <img src={isShowing ? `${basePath}${item.image}` : backCard} alt={item.name} className={`${style.img}`} />
-            </button>
-          </div>
-          <div
-            key="back"
-            data-tip={isShowing ? '' :
-            `${item.name} <br/> <br/> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum`}
-            data-multiline
-          >
-            <button
-              onClick={() => this.checkOneOpened()}
-            >
-              <img src={isShowing ? backCard : `${basePath}${item.image}`} alt={item.name} className={`${style.img}`} />
-            </button>
-          </div>
-        </ReactCardFlip>
-        <ReactTooltip />
-      </div>
-    );
-  }
+	checkOneOpened(e) {
+		if (!this.props.isShowing) {
+			this.handleClick(e);
+		}
+	}
+
+	render() {
+		const { item, basePath, backCard, isShowing } = this.props;
+		return (
+			<div className={`col-lg-2 col-md-3 col-sm-6 ${style.card}`}>
+				<ReactCardFlip
+					isFlipped={this.state.isFlipped}
+					flipSpeedBackToFront={0.9}
+					flipSpeedFrontToBack={0.9}
+				>
+					<div
+						key="front"
+						data-tip={isShowing ?
+						`${item.name} <br/> <br/> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum` :
+						'clique para descobrir'}
+						data-multiline
+					>
+						<button
+							onClick={() => this.checkOneOpened()}
+						>
+							<img src={isShowing ? `${basePath}${item.image}` : backCard} alt={item.name} className={`${style.img}`} />
+						</button>
+					</div>
+					<div
+						key="back"
+						data-tip={isShowing ? '' :
+						`${item.name} <br/> <br/> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum`}
+						data-multiline
+					>
+						<button
+							onClick={() => this.checkOneOpened()}
+						>
+							<img src={isShowing ? backCard : `${basePath}${item.image}`} alt={item.name} className={`${style.img}`} />
+						</button>
+					</div>
+				</ReactCardFlip>
+				<ReactTooltip />
+			</div>
+		);
+	}
 }
 
 export default Card;
 
 
 Card.propTypes = {
-  basePath: PropTypes.string,
-  backCard: PropTypes.string,
-  isShowing: PropTypes.bool,
-  historyToggleStates: PropTypes.func,
-  isFlipped: PropTypes.bool,
-  checkOneOpened: PropTypes.func,
-  item: PropTypes.func,
+	basePath: PropTypes.string,
+	backCard: PropTypes.string,
+	isShowing: PropTypes.bool,
+	historyToggleStates: PropTypes.func,
+	checkOneOpened: PropTypes.func,
+	item: PropTypes.object,
 };
--- app/components/container/container.js
+++ app/components/container/container.js
@@ -2,54 +2,53 @@ import React, { PropTypes, Component } from 'react';
 import Card from './card/card.js';
 
 class Container extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFlipped: false,
-      history: [],
-    };
-    this.historyToggleStates = this.historyToggleStates.bind(this);
-  }
-  historyToggleStates(bool, id) {
-    this.setState({
-      history: this.state.history.concat([{ opened: bool, id }]),
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			countClicks: 0,
+			cardsState: true,
+			history: [],
+		};
+		this.historyToggleStates = this.historyToggleStates.bind(this);
+	}
+	historyToggleStates() {
+		this.setState({ countClicks: this.state.countClicks + 1, cardsState: !this.state.cardsState });
+	}
 
-  render() {
-    const rest = {
-      basePath: this.props.basePath,
-      backCard: this.props.backCard,
-      isShowing: this.props.isShowing,
-      historyToggleStates: this.historyToggleStates,
-      isFlipped: this.state.isFlipped,
-      checkOneOpened: this.checkOneOpened,
-      history: this.state.history,
+	render() {
+		const rest = {
+			basePath: this.props.basePath,
+			backCard: this.props.backCard,
+			isShowing: this.props.isShowing,
+			historyToggleStates: this.historyToggleStates,
+			checkOneOpened: this.checkOneOpened,
+			history: this.state.history,
+			cardsState: this.state.cardsState,
 
-    };
-    const cardsMap = this.props.cards.map((item, key) => {
-      return (
-        <Card
-          item={item}
-          keyId={key}
-          {...rest}
-        />
-      );
-    });
-    return (
-      <div className="col-lg-12 text-center">
-        {cardsMap}
-      </div>
-    );
-  }
+		};
+		const cardsMap = this.props.cards.map((item, key) => {
+			return (
+				<Card
+					item={item}
+					keyId={key}
+					{...rest}
+				/>
+			);
+		});
+		return (
+			<div className="col-lg-12 text-center">
+				{cardsMap}
+			</div>
+		);
+	}
 }
 
 export default Container;
 
 Container.propTypes = {
-  cards: PropTypes.array.isRequired,
-  item: PropTypes.func,
-  basePath: PropTypes.string,
-  backCard: PropTypes.string,
-  isShowing: PropTypes.bool,
+	cards: PropTypes.array.isRequired,
+	item: PropTypes.func,
+	basePath: PropTypes.string,
+	backCard: PropTypes.string,
+	isShowing: PropTypes.bool,
 };
